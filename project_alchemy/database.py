8e11fc15a8d1a2b0d676f2efaae068dd58d676b4
nurzhank <asapnrz@gmail.com>
2024-04-09 17:40:47 +0500

    commit upd

--- project_alchemy/database.py
+++ project_alchemy/database.py
@@ -1,11 +1,16 @@
+import os
 from sqlalchemy import (
     create_engine
 )
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+from dotenv import load
+
+load()
+LOGIN = os.getenv('LOGIN')
 
 engine = create_engine(
-    'postgresql+psycopg2://postgres:1234@localhost/python17_2',
+    LOGIN,
     echo=True
 )
 
