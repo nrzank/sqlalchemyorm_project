8f593feab89c084e97b357b413211486a7ad2486
nurzhank <asapnrz@gmail.com>
2024-04-10 12:22:49 +0500

    database upd

--- project_alchemy/database.py
+++ project_alchemy/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import (
     create_engine
 )
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from dotenv import load
 
@@ -14,5 +14,3 @@ engine = create_engine(
 )
 
 Session = sessionmaker(bind=engine)
-
-Base = declarative_base()
