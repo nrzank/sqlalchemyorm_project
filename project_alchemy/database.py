9132e31c7f566a9ccaabeb3ea84000a7cc83c65d
nurzhank <asapnrz@gmail.com>
2024-04-20 15:00:12 +0500

    upd database

--- project_alchemy/database.py
+++ project_alchemy/database.py
@@ -1,12 +1,15 @@
+import os
+from dotenv import load
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
+load()
+URL = os.getenv('URL_STRING')
 engine = create_engine(
-    "postgresql+psycopg2://postgres:1234@localhost/python17_2",
+    URL
     # echo=True
 )
 
-Session = sessionmaker(engine)
+Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
